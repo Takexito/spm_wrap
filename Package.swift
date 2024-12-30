0e3104f58759415fdeb2c1df1868bdc0f98db490
Andrey Yavorsky <andrey@hut.dev>
2024-12-30 11:32:57 +0300

    add lib

--- Package.swift
+++ Package.swift
@@ -5,17 +5,43 @@ import PackageDescription
 
 let package = Package(
     name: "WrapperSPM",
+    platforms: [
+        .macOS(.v10_13), .macCatalyst(.v13), .iOS(.v12), .tvOS(.v12), .watchOS(.v4), .custom("visionos", versionString: "1.0")
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
+        // Объединённая обёртка
         .library(
             name: "WrapperSPM",
-            targets: ["WrapperSPM"]),
+            type: .dynamic,
+            targets: ["WrapperSPM"]
+        )
+    ],
+    dependencies: [
+        // Подключение всех библиотек как зависимости
+        .package(url: "https://github.com/airsidemobile/JOSESwift", from: "2.4.0"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.8.2"),
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.19.3"),
+        .package(url: "https://github.com/roberthein/TinyConstraints.git", from: "4.0.2")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        // Таргет для объединённой обёртки
         .target(
-            name: "WrapperSPM"),
-
-    ]
+            name: "WrapperSPM",
+            dependencies: [
+                .product(name: "JOSESwift", package: "JOSESwift"),
+                .product(name: "CryptoSwift", package: "CryptoSwift"),
+                .product(name: "SDWebImage", package: "SDWebImage"),
+                .product(name: "TinyConstraints", package: "TinyConstraints")
+            ],
+            path: "Sources/WrapperSPM"
+        )
+    ],
+    swiftLanguageModes: [.v5]
 )
+
+// Поддержка DocC для документации
+#if swift(>=5.6)
+package.dependencies.append(
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0")
+)
+#endif
